740d70cbb53ea9760cfd1ad3f1db0b8de5a5f00f
alishair7071 <alishair7071.com>
2026-07-03 21:38:35 +0500

    updated

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.post("/add-message", (req, res) => {
 
 
 app.get("/test", (req, res) => {
-  res.json({ message: "Test route is working!" });
+  res.json({ message: "Test route is working! updated" });
 });
 
 app.listen(PORT, () => {
