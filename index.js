4b9e2c1d08a8ab79186c7a52f36e206c2eacc526
alishair7071 <alishair7071.com>
2026-07-07 18:57:48 +0500

    abcd

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.post("/add-message", (req, res) => {
 
 
 app.get("/test", (req, res) => {
-  res.json({ message: "Test route is working! updated now urgently mmmmmmm" });
+  res.json({ message: "Test route is working! updated now urgently abcdefghijklmnop" });
 });
 
 app.listen(PORT, () => {
