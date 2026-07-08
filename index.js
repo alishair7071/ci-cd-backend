8512a667a3d87cd9e03c548f2eee6d98eafb3f85
alishair7071 <alishair7071.com>
2026-07-08 16:27:08 +0500

    rrrrrrr

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.post("/add-message", (req, res) => {
 
 
 app.get("/test", (req, res) => {
-  res.json({ message: "Test route is working! now automatexxxxxxxxxyyy" });
+  res.json({ message: "Test route is working! now automaterrrrrrrrrrr" });
 });
 
 app.listen(PORT, () => {
